ea2bec842aaa4e328ae1f654b8f0625faee1d2ca
Suvrath Penmetcha <spenmetc@andrew.cmu.edu>
2016-04-04 14:41:06 -0400

    made progress on http request but still needs to figure out username and authentication

--- request.js
+++ request.js
@@ -2,33 +2,68 @@
 var url = 'https://128.2.109.159/piwebapi/streams/P0-MYhSMORGkyGTe9bdohw0ArhsBAAV0lOLTYyTlBVMkJWTDIwXFBISVBQU19FTEVDIEhWQUMgQUxMIENTTA/plot';
 var response = null;
 /* GET request */
-function getData(){
-	$.ajax({
-    'type': 'GET',
-    'url': url,
-    'crossDomain': 'True',
-    'headers': { 'Access-Control-Allow-Origin': '*' },
-    // 'Username': 'Phipps_IS',
-    // 'Password': 'Energy1?',
-    // 'xhrFields': {
-    //     'withCredentials': true
-    // }, //only required for CORS
-    'success': function(response){
-      /* do something with data returned */
-      response = response;
-      console.log(response);
-      // var valueURL = response.Links.Value;
-    }
-	});
-}
-$( document ).ready(function() {
-});
-// function getData(){
-//   $.getJSON('https://128.2.109.159/piwebapi/streams/P0-MYhSMORGkyGTe9bdohw0ArhsBAAV0lOLTYyTlBVMkJWTDIwXFBISVBQU19FTEVDIEhWQUMgQUxMIENTTA/plot', 
-//     function(json) {
-//       console.log(json);
+
+//   $.ajax({
+//     type: "GET",
+//     xhrFields: {
+//         withCredentials: true
+//     },
+//     dataType: "jsonp",
+//     contentType: "application/javascript",
+//     async: false,
+//     crossDomain: true,
+//     url: "https://128.2.109.159/piwebapi/streams/P0-MYhSMORGkyGTe9bdohw0ArhsBAAV0lOLTYyTlBVMkJWTDIwXFBISVBQU19FTEVDIEhWQUMgQUxMIENTTA/plot",
+//     success: function (jsonData) {
+//         console.log(jsonData);
+//     },
+//     error: function (request, textStatus, errorThrown) {
+//         console.log(request.responseText);
+//         console.log(textStatus);
+//         console.log(errorThrown);
+//     },
+//     username: username,
+//     password: password,
 //   });
-// }
+//   alert("finished button function");
+// });
 
-// https://128.2.109.159/piwebapi/streams/P0-MYhSMORGkyGTe9bdohw0ArhsBAAV0lOLTYyTlBVMkJWTDIwXFBISVBQU19FTEVDIEhWQUMgQUxMIENTTA/plot
+function callbackMethod(saveData){
+ if (saveData == null) {
+    alert("DATA IS UNDEFINED!");  // displays every time
+  }
+  console.log(saveData);
+}
 
+$( document ).ready(function() {
+  // $('#buttonGraph').on('click', function() {
+  //   alert("hello");
+  // });
+  $('#buttonGraph').on('click', function() {
+    $.ajax({
+      type: "GET",
+      xhrFields: {
+          withCredentials: true
+      },
+      dataType: "jsonp",
+      contentType: "application/javascript",
+      async: true,
+      crossDomain: true,
+      url: "https://128.2.109.159/piwebapi/streams/P0-MYhSMORGkyGTe9bdohw0ArhsBAAV0lOLTYyTlBVMkJWTDIwXFBISVBQU19FTEVDIEhWQUMgQUxMIENTTA/plot?callback=?",
+      jsonpCallback: 'callbackMethod',
+      success: function (jsonData) {
+          console.log("entered success");
+          var data = JSON.parse(jsonData)
+          console.log("Success");
+          console.log(data);
+      },
+      error: function (request, textStatus, errorThrown) {
+          console.log("Failure")
+          console.log(request.responseText);
+          console.log(textStatus);
+          console.log(errorThrown);
+      },
+      username: 'Phipps_IS',
+      password: 'Energy1?',
+    });
+  });
+});
