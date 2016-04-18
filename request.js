6dc0256724808ed31871a54bedaff29090c4a721
Suvrath Penmetcha <spenmetc@andrew.cmu.edu>
2016-04-17 23:05:06 -0400

    data not hardcoded

--- request.js
+++ request.js
@@ -1,42 +1,10 @@
 /* construct URL */
 var url = 'https://128.2.109.159/piwebapi/streams/P0-MYhSMORGkyGTe9bdohw0ArhsBAAV0lOLTYyTlBVMkJWTDIwXFBISVBQU19FTEVDIEhWQUMgQUxMIENTTA/plot';
 var response = null;
-/* GET request */
-
-//   $.ajax({
-//     type: "GET",
-//     xhrFields: {
-//         withCredentials: true
-//     },
-//     dataType: "jsonp",
-//     contentType: "application/javascript",
-//     async: false,
-//     crossDomain: true,
-//     url: "https://128.2.109.159/piwebapi/streams/P0-MYhSMORGkyGTe9bdohw0ArhsBAAV0lOLTYyTlBVMkJWTDIwXFBISVBQU19FTEVDIEhWQUMgQUxMIENTTA/plot",
-//     success: function (jsonData) {
-//         console.log(jsonData);
-//     },
-//     error: function (request, textStatus, errorThrown) {
-//         console.log(request.responseText);
-//         console.log(textStatus);
-//         console.log(errorThrown);
-//     },
-//     username: username,
-//     password: password,
-//   });
-//   alert("finished button function");
-// });
-
-// function callbackMethod(saveData){
-//  if (saveData == null) {
-//     alert("DATA IS UNDEFINED!");  // displays every time
-//   }
-//   console.log(saveData);
-// }
-
 
 var hvacData = {};
 $( document ).ready(function() {
+  ajaxRequest();
   $(function () {
     $('#container').highcharts({
       title: {
@@ -44,9 +12,7 @@ $( document ).ready(function() {
         x: -20 //center
       },
       xAxis: {
-        categories: ['04-11 12:27am', '04-11 12:49am', '04-11 12:50am', '04-11 12:54am', '04-11 1:40am', 
-        '04-11 1:49am', '04-11 1:50am', '04-11 1:57am', '04-11 2:47am', '04-11 2:49am', '04-11 2:50am', 
-        '04-11 3:33am']
+        categories: hvacData.categories;
       },
       yAxis: {
         title: {
@@ -69,8 +35,7 @@ $( document ).ready(function() {
       },
       series: [{
         name: 'Electrical HVAC',
-        data: [19.4921, 18.2377, 17.7033, 18.59525, 0.4557355, 0.478894, 0.4798295, 
-        0.4564605, 17.43555, 19.2297, 19.8089, 0.456781]
+        data: hvacData.dataArray;
       }]
     });
   });
@@ -91,7 +56,7 @@ $( document ).ready(function() {
     return hvacData;
   }
   
-   $('#buttonGraph').on('click', function() {
+   function ajaxRequest() {
     var tok = 'Phipps_IS' + ':' + 'Energy1?';
     hash = btoa(tok);
     authInfo = "Basic " + hash;
@@ -127,5 +92,5 @@ $( document ).ready(function() {
           console.log(errorThrown);
       }
     });
-  });
+  };
 });
