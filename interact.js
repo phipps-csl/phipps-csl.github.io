62a77fd49ef5bae2329707ba8f7adb66be718a45
Alex Tsai <alextsai@andrew.cmu.edu>
2016-04-18 11:13:08 -0400

    prettier multiple choice interaction

--- interact.js
+++ interact.js
@@ -1,12 +1,11 @@
  function check(button) {
     if (button.className == "incorrect")
     {
-      button.style.backgroundColor = "#E66E74";
+      button.style.borderColor = "#E66E74";
       
     }
     else
     {
-      button.style.backgroundColor = "#96D5AF";
+      button.style.borderColor = "#96D5AF";
     }
-    button.style.color = "#FFF"; 
   }
