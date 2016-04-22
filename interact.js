990975ee4d9363b1eebd4cc5a750a861790726ed
Alex Tsai <alextsai@andrew.cmu.edu>
2016-04-21 22:07:49 -0400

    strikethrough for colorblind

--- interact.js
+++ interact.js
@@ -2,10 +2,13 @@
     if (button.className == "incorrect")
     {
       button.style.borderColor = "#E66E74";
+      button.style.textDecoration = "line-through";
+      button.style.color = "#E66E74";
       
     }
     else
     {
       button.style.borderColor = "#96D5AF";
+      button.style.color = "#96D5AF";
     }
   }
